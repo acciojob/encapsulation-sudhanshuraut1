213a1e63a6a7ac9a09412f2b07f2df1edf738a55
Sudhanshu raut <sudhanshuraut1@gmail.com>
2023-01-21 03:11:36 +0530

    encapsulation 1st attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly a = new RWOnly();
+        a.setName("sudha");
+        a.getName();
+
+    }
 }
